69fb49bce9f1d53e5a444fa113a56e4731c832eb
juaninga <juan.inga@unae.edu.ec>
2023-04-26 09:21:33 -0500

    routing

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const HomeScreen = () => {
+    return (  
+        <View>
+            <Text style={{ fontSize: 42 }}>Vamos desde el main seguro</Text>
+        </View>
+    );
+}
+ 
+export default HomeScreen;
---
+++ src/screens/SigninScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+const Signincreen = () => {
+    return (  
+        <View>
+            <Text style={{ fontSize: 42 }}>Vamos desde el main</Text>
+        </View>
+    );
+}
+ 
+export default Signincreen;
