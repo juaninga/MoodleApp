ede26e51f01d71ed499c73d05c17a6f6307bf022
juaninga <juan.inga@unae.edu.ec>
2023-04-26 12:53:50 -0500

    iconos

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const HomeScreen = () => {
     return (  
         <View>
             <Text style={{ fontSize: 42 }}>Vamos desde el main seguro</Text>
+            <Icon name="home" size={30} color="red" />
         </View>
     );
 }
