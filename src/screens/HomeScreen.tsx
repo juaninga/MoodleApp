2121fca000e36670c474a9a63f3592de8ce0fa7c
Juan Carlos Reinoso <juan_karlos_1992@hotmail.com>
2023-04-26 20:14:53 -0500

    Creacion de Splash

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet, SafeAreaView } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import LottieContainer,{
+  LottieContainerProps,
+ } from '../components/LottieContainer';
+import Animated, { FadeIn } from 'react-native-reanimated';
 
-const HomeScreen = () => {
-    return (
-        <View>
-            <Text style={{ fontSize: 42 }}>Vamos desde el main seguro</Text>
-          <Text style={{ fontSize: 42 }}>Sin Miedo al Exito</Text>
-            <Icon name="book" size={50} color="black" />
-        </View>
-    );
+export default function HomeScreen(): JSX.Element {
+  return (
+    <SafeAreaView style={{backgroundColor: '#F6AD76'}}>
+      <Animated.ScrollView entering={FadeIn.duration(500)}>
+        <Text style={styles.title}>Bienvenidos a UNAE</Text>
+        {animations.map((animation, index) => (
+          <LottieContainer
+            key={index}
+            {...animation} />
+        ))}
+      </Animated.ScrollView>
+    </SafeAreaView>
+  );
 }
 
-export default HomeScreen;
+const styles = StyleSheet.create({
+  title: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color:'white',
+  },
+});
+
+const animations: LottieContainerProps[] = [
+  {
+    name:'Sucess',
+    source: 'https://lottiefiles.com/782-check-mark-success',
+    author: 'Travis Gregory',
+    path: require('../assets/splash.json'),
+  },
+];
+
+
