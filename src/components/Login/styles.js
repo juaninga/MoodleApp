5a11b9229ae3f9bc5be65fa21dfd7269ffd68bac
juaninga <juan.inga@unae.edu.ec>
2023-04-28 08:21:07 -0500

    cambios

--- src/components/Login/styles.js
+++ src/components/Login/styles.js
@@ -37,6 +37,25 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     paddingLeft: 10
   },
+  formButton: {
+    backgroundColor: 'rgba(123,104,238,0.8)',
+    height: 55,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 35,
+    marginHorizontal: 20,
+    marginVertical: 10,
+    borderWidth: 1,
+    borderColor: 'white',
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 4,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
 
   formInputContainer: {
     marginBottom: 70,
